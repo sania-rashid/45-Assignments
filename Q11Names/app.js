071ce2c74251b05d66301f2ee510836db106a157
Sania Rashid <officialsania113@gmail.com>
2024-05-02 19:09:03 +0500

    push to github

---
+++ Q11Names/app.js
@@ -0,0 +1,4 @@
+var members = ['Sania', 'Fariha', 'Maryum', 'shahmeen', 'Aleena'];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ Q11Names/app.ts
@@ -0,0 +1,5 @@
+let members: string[] =['Sania','Fariha','Maryum','shahmeen','Aleena'];
+for(let i=0; i<members.length; i++){
+  console.log(members[i]); 
+}
+
