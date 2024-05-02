337dbe3433564e91941f18ca219a57ce9498165f
Sania Rashid <officialsania113@gmail.com>
2024-05-02 19:18:34 +0500

    push my code

---
+++ Q13Your own array/app.js
@@ -0,0 +1,4 @@
+var transportation = ['Haval H6 hev', 'Double Cabin', 'Heavy Bike', 'Scooty', 'Hiroof'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
+}
---
+++ Q13Your own array/app.ts
@@ -0,0 +1,4 @@
+ let transportation: string[]=['Haval H6 hev','Double Cabin','Heavy Bike','Scooty','Hiroof'];
+for(let i=0; i<transportation.length; i++){
+    console.log('I would like to own a '  + transportation[i]);
+}
