8d478238ecceed2bd759104aff2f9dfbf725f1f9
Sania Rashid <officialsania113@gmail.com>
2024-05-02 18:57:26 +0500

    push to github

---
+++ Q10 Adding Comment/app.js
@@ -0,0 +1,5 @@
+//excercise10
+//my name is SANIA
+//Dated:26/2/2024
+//this program will run simple code just like hello world
+console.log('Hello World simple program');
---
+++ Q10 Adding Comment/program.js
@@ -0,0 +1,5 @@
+//excercise10
+//my name is SANIA
+//Dated:26/2/2024
+//this program will multiplication
+console.log(5 * 2);
