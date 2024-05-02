c7c92a717e8584a3a4247d0a58ce31b3b2b37e25
Sania Rashid <officialsania113@gmail.com>
2024-05-02 19:15:58 +0500

    push to github

---
+++ Q12Greeting/app.js
@@ -0,0 +1,5 @@
+var members = ['Sania', 'Fariha', 'Okasha', 'Huzaifa'];
+var message = 'You are a good student:';
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ Q12Greeting/app.ts
@@ -0,0 +1,5 @@
+let members: string[]=['Sania','Fariha','Okasha','Huzaifa'];
+let message: string='You are a good student:';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
