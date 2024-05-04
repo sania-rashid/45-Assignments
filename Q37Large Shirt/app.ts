b21215b35045898502ae6a3716988489fc2ee87d
Sania Rashid <officialsania113@gmail.com>
2024-05-04 19:37:41 +0500

    push to github

---
+++ Q37Large Shirt/app.ts
@@ -0,0 +1,12 @@
+function make_shirt(size = 'large', message = 'I love TypeScript') {
+    console.log(`The shirt size is ${size} and it says: ${message}`);
+}
+
+
+make_shirt();
+
+
+make_shirt('medium');
+
+
+make_shirt('small', 'TypeScript is awesome!');
