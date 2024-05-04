8ab4da14fa12ef976e1aeb15790815c2df641205
Sania Rashid <officialsania113@gmail.com>
2024-05-04 18:45:41 +0500

    push to github

---
+++ Q23Conditional Test/app.js
@@ -0,0 +1,32 @@
+//let car = 'Mercedes Benz';
+//console.log("Is car == 'Mercedes Benz'? I predict True.")
+//console.log(car == 'Mercedes Benz')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var num = 15;
+//console.log("Is num > 10? I predict True.");
+//console.log(num > 10); // True
+//console.log("Is num < 5? I predict False.");
+//console.log(num < 5); // False
+var fruit = 'banana';
+//console.log("Is fruit == 'apple'? I predict False.");
+//console.log(fruit == 'apple'); // False
+//console.log("Is fruit != 'orange'? I predict True.");
+//console.log(fruit != 'orange'); // True
+var isTrue = true;
+//console.log("Is isTrue == true? I predict True.");
+//console.log(isTrue == true); // True
+//console.log("Is isTrue == false? I predict False.");
+//console.log(isTrue = false); // False
+var x;
+//console.log("Is x defined? I predict False.");
+//console.log(typeof x !== 'undefined'); // False
+var y = null;
+//console.log("Is y null? I predict True.");
+//console.log(y === null); // True
+var z = 8;
+//console.log("Is z between 1 and 10? I predict True.");
+//console.log(z > 1 && z < 10); // True
+var str = 'hello';
+//console.log("Does str contain the letter 'e'? I predict True.");
+//console.log(str.includes('e')); // True
