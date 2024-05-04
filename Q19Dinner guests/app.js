fa7ee724d8bc7f835cec58163eccd6a95dc8af54
Sania Rashid <officialsania113@gmail.com>
2024-05-04 18:42:13 +0500

    push to github

---
+++ Q19Dinner guests/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_1 = require("../Q14Guest list/app");
+console.log('\n printing number of guest invited.\n');
+console.log("we had finally invited ".concat(app_1.guest_list.length, " people"));
---
+++ Q19Dinner guests/app.ts
@@ -0,0 +1,3 @@
+import { guest_list } from '../Q14Guest list/app';
+console.log('\n printing number of guest invited.\n');
+console.log(`we had finally invited ${guest_list.length} people`);
