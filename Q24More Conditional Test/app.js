4393418a0f4e77425b97c3c566a6187f8dd087b4
Sania Rashid <officialsania113@gmail.com>
2024-05-04 18:47:16 +0500

    push to github

---
+++ Q24More Conditional Test/app.js
@@ -0,0 +1,34 @@
+//• Tests for equality and inequality with strings
+//• Tests using the lower case function
+//• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+//• Tests using "and" and "or" operators
+//• Test whether an item is in a array
+//• Test whether an item is not in a array
+var fruit1 = 'apple';
+var fruit2 = 'orange';
+//console.log("Are fruit1 and fruit2 equal? I predict False.");
+//console.log(fruit1 == fruit2); // False
+//console.log("Are fruit1 and fruit2 not equal? I predict True.");
+//console.log(fruit1 != fruit2); // True
+var str1 = 'HELLO';
+var str2 = 'hello';
+//console.log("Are str1 and str2 equal when both are converted to lowercase? I predict True.");
+//console.log(str1.toLowerCase() == str2.toLowerCase()); // True
+var num1 = 5;
+var num2 = 10;
+//console.log("Is num1 less than num2? I predict True.");
+//console.log(num1 < num2); // True
+//console.log("Is num1 greater than or equal to num2? I predict False.");
+//console.log(num1 >= num2); // False
+var x = 10;
+var y = 20;
+//console.log("Is x greater than 5 and less than 15? I predict True.");
+//console.log(x > 5 && x < 15); // True
+//console.log("Is x less than 5 or greater than 15? I predict False.");
+//console.log(x < 5 || x > 15); // False
+var colors = ['red', 'green', 'blue'];
+var color = 'yellow';
+//console.log("Is color in the colors array? I predict False.");
+//console.log(colors.includes(color)); // False
+//console.log("Is color not in the colors array? I predict True.");
+//console.log(!colors.includes(color)); // True
---
+++ Q25Alien Colors/app.ts
@@ -0,0 +1,21 @@
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+//• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+
+
+
+
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+    //console.log("Congratulations! You just earned 5 points.");
+}
+
+
+
+let aliencolor = 'red';
+
+if (alien_color === 'green') {
+    //console.log("Congratulations! You just earned 5 points.");
+}
