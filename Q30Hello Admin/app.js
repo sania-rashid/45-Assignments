a4ef009add6f14c209cbbe012a805680d1156ed2
Sania Rashid <officialsania113@gmail.com>
2024-05-04 18:50:07 +0500

    push to github

---
+++ Q30Hello Admin/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let usernames = ["saqib", "aziz", "fahad", "waqar", "siraj"];
+for (let i = 0; i < usernames.length; i++) {
+    if (usernames[i] === 'saqib') {
+        //console.log("Hello saqib, would you like to see a status report?");
+    }
+    else {
+        //console.log(`Hello ${usernames[i]}, thank you for logging in again.`);
+    }
+}
---
+++ Q30Hello Admin/app.ts
@@ -0,0 +1,11 @@
+let usernames = ["saqib", "aziz", "fahad", "waqar", "siraj"];
+
+
+for (let i = 0; i < usernames.length; i++) {
+    
+    if (usernames[i] === 'saqib') {
+        //console.log("Hello saqib, would you like to see a status report?");
+    } else {
+        //console.log(`Hello ${usernames[i]}, thank you for logging in again.`);
+    }
+}
