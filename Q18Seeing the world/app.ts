d4902a5efcb08ac9daca0b7f0824d59a42caa2f2
Sania Rashid <officialsania113@gmail.com>
2024-05-04 18:41:21 +0500

    push to github

---
+++ Q18Seeing the world/app.ts
@@ -0,0 +1,9 @@
+let places: string[]=['Canada','Greece','Europe','Austraila','Maldives'];
+//console.log('original ' + places);
+ //console.log('copy '  + [...places].sort());
+ //console.log('original ' + places);
+
+console.log('copy ' + [...places].sort().reverse());
+console.log('copy ' + [...places].sort().reverse());
+console.log('original ' +places.sort());
+console.log('original ' +places.sort().reverse());
