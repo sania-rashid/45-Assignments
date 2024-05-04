ea7cd4444bc1b2d9618edb0969185eef80f9c3aa
Sania Rashid <officialsania113@gmail.com>
2024-05-04 18:55:06 +0500

    push to github

---
+++ Q36T-shirt/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`The shirt size is ${size} and it says: ${message}`);
+}
+make_shirt('large', 'I love TypeScript!');
---
+++ Q36T-shirt/app.ts
@@ -0,0 +1,6 @@
+function make_shirt(size:string, message:string) {
+    //console.log(`The shirt size is ${size} and it says: ${message}`);
+}
+
+
+make_shirt('large', 'I love TypeScript!');
