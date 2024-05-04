1e7847c1dff009ec0f9865c2b671a6db77d5a2fb
Sania Rashid <officialsania113@gmail.com>
2024-05-04 19:32:57 +0500

    push to github

--- Q44Sandwiches/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function make_sandwich(...items) {
-    console.log("Making a sandwich with the following items:");
-    items.forEach(item => console.log("- " + item));
-    console.log("Enjoy your sandwich!");
-}
-make_sandwich("Ham", "Cheese", "Lettuce");
-make_sandwich("Turkey", "Swiss");
-make_sandwich("Peanut Butter", "Jelly");
